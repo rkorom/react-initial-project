fa1213e81d01bcafed21cc1687dae89633f12be7
rkorom <richard.korom@gmail.com>
2025-01-15 09:12:45 +0100

    Remove jest-dom dependency and update index.tsx to render "Hello World!"

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(<React.StrictMode>Hello World!</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
