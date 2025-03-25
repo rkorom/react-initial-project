5cd371a5ae1a35f83bd4b31dc4dc97535517edd5
rkorom <richard.korom@gmail.com>
2025-03-25 10:29:12 +0100

    Add testing library and initial test for App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
-
-root.render(<React.StrictMode>Hello World!</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
