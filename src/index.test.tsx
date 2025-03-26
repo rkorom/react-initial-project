facbd6cb57f376bded76bbf7711eeb72e70cfc87
Richard Korom <57049330+rkorom@users.noreply.github.com>
2025-03-26 07:41:30 +0100

    Remove index.test.tsx as part of test suite cleanup

--- src/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import '@testing-library/jest-dom';
-
-test('a / útvonalon megjelenik a Hello World szöveg', () => {
-    render(<App />);
-    expect(screen.getByText('Hello World!')).toBeInTheDocument();
-});
