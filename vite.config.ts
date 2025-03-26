36363575739aff0bd1e08d411710e112aba7c116
Richard Korom <57049330+rkorom@users.noreply.github.com>
2025-03-26 07:43:53 +0100

    Update Vite configuration to set project root to current directory

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    root: 'src', // A src mappa lesz a gyökér
+    root: '.', // A src mappa lesz a gyökér
     plugins: [react()],
     test: {
         globals: true,
